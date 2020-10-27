6b994c94905d6268d392dd974a315de572133a39
nikeeshi <kenta.kf@gmail.com>
2020-10-28 00:51:26 +0900

    auto darkmode

--- components/context/theme.tsx
+++ components/context/theme.tsx
@@ -1,11 +1,17 @@
 import { createContext, useState } from "react";
+import { getHours } from "date-fns";
+const isNightNow = () => {
+  const now = new Date();
+  const hour = getHours(now);
+  return hour < 6 || 18 < hour;
+};
 
 export const Theme = createContext({
   darkMode: false,
   toggleDarkMode: () => {},
 });
 export const ThemeProvider: React.FC = ({ children }) => {
-  const [darkmode, setDarkmode] = useState(false);
+  const [darkmode, setDarkmode] = useState(isNightNow());
   return (
     <Theme.Provider
       value={{
